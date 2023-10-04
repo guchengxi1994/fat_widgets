9288268e746b00eddaac624aa78b330b38cbbfaf
xiaoshuyui <528490652@qq.com>
2023-10-04 09:44:43 +0800

    Update column_item.dart

--- lib/table/column_item.dart
+++ lib/table/column_item.dart
@@ -2,13 +2,18 @@ import 'package:flutter/material.dart';
 
 typedef OnSort = void Function(bool ascending);
 
-class ColumnItem extends StatelessWidget {
-  ColumnItem({Key? key, required this.label, this.tooltip, this.onSort})
+class ColumnItem extends StatefulWidget {
+  const ColumnItem({Key? key, required this.label, this.tooltip, this.onSort})
       : super(key: key);
   final Widget label;
   final String? tooltip;
   final OnSort? onSort;
 
+  @override
+  State<ColumnItem> createState() => _ColumnItemState();
+}
+
+class _ColumnItemState extends State<ColumnItem> {
   final ValueNotifier<bool> notifier = ValueNotifier(false);
 
   @override
@@ -18,19 +23,19 @@ class ColumnItem extends StatelessWidget {
         builder: (c, b, w) {
           return Material(
             child: InkWell(
-              onTap: onSort != null
+              onTap: widget.onSort != null
                   ? () {
                       notifier.value = !notifier.value;
-                      onSort!(notifier.value);
+                      widget.onSort!(notifier.value);
                     }
                   : null,
               child: FittedBox(
                 child: Tooltip(
-                  message: tooltip ?? "",
+                  message: widget.tooltip ?? "",
                   child: Row(
                     children: [
-                      label,
-                      if (onSort != null)
+                      widget.label,
+                      if (widget.onSort != null)
                         notifier.value
                             ? const Icon(Icons.arrow_downward)
                             : const Icon(Icons.arrow_upward)
